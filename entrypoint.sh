35f615c20845cebe29f18bf0e7073d5184404ac9
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-04-12 22:14:15 +0100

    Remove debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 echo "JAVA_HOME is ${JAVA_HOME}"
-echo "Using galasactl version ${INPUT_GALASACTL_VERSION}"
+echo "Using galasactl version '${INPUT_GALASACTL_VERSION}'"
 echo "Running command: galasactl ${INPUT_GALASACTL_ARGS}"
-
-pwd
-ls -lah
-ls -lah .m2/repository
-ls -lah .galasa
 
 galasactl ${INPUT_GALASACTL_ARGS}
