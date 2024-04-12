0e9d9701d62fe892fad56e6bf84133ae4f800479
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-04-12 19:41:26 +0100

    Switch to downloading galasactl in Dockerfile
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
-FROM docker:26.0.1
-
-RUN apk add bash
+FROM eclipse-temurin:11
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x entrypoint.sh
+
+RUN apt-get update \
+    && apt-get install -y fontconfig libfreetype6
+
+RUN curl -L -o /usr/bin/galasactl https://github.com/galasa-dev/cli/releases/download/v0.32.0/galasactl-linux-x86_64 \
+    && chmod 755 /usr/bin/galasactl
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,4 @@
 echo "Using galasactl version ${INPUT_GALASACTL_VERSION}"
 echo "Running command: galasactl ${INPUT_GALASACTL_ARGS}"
 
-pwd
-ls -lah
-
-exec docker run \
-    -v "/var/run/docker.sock:/var/run/docker.sock" \
-    -v "$(pwd):/root/.galasa" \
-    --entrypoint="/bin/galasactl" \
-    --user="root" \
-    icr.io/galasadev/galasa-cli-amd64:${INPUT_GALASACTL_VERSION} ${INPUT_GALASACTL_ARGS}
+galasactl ${INPUT_GALASACTL_ARGS}
