4e07c4b830038bcc6dbb6eac4ce83ea4e664ce27
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-04-12 16:36:15 +0100

    Run galasactl as the galasa user
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ echo "Running command: galasactl ${INPUT_GALASACTL_ARGS}"
 exec docker run \
     -v "/var/run/docker.sock:/var/run/docker.sock" \
     --entrypoint="/bin/galasactl" \
+    --user="galasa" \
     icr.io/galasadev/galasa-cli-amd64:${INPUT_GALASACTL_VERSION} ${INPUT_GALASACTL_ARGS}
