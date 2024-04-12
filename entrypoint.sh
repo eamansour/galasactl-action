43be9da111eae7a6bb9d27ff11e7405d966974fb
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-04-12 21:56:23 +0100

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,9 @@ echo "JAVA_HOME is ${JAVA_HOME}"
 echo "Using galasactl version ${INPUT_GALASACTL_VERSION}"
 echo "Running command: galasactl ${INPUT_GALASACTL_ARGS}"
 
+pwd
+ls -lah
+ls -lah .m2/repository
+ls -lah .galasa
+
 galasactl ${INPUT_GALASACTL_ARGS}
