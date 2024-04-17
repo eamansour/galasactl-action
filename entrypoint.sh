80bc64b2d4e0ff02e38a344b307d6d933b786e0a
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-04-17 18:33:35 +0100

    Add eval

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Using galasactl version '${INPUT_GALASACTL_VERSION}'"
 echo "Running command: galasactl ${INPUT_GALASACTL_ARGS}"
 
 if [ -z "${INPUT_GALASACTL_ARGS}" ]; then
-    ${INPUT_SCRIPT}
+    eval "${INPUT_SCRIPT}"
 else
     galasactl ${INPUT_GALASACTL_ARGS}
 fi
