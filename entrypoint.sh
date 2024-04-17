96ff09863d65ecc4c22b4e83683b2a9e00a86fbf
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-04-17 16:46:14 +0100

    Add script parameter
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@ echo "JAVA_HOME is ${JAVA_HOME}"
 echo "Using galasactl version '${INPUT_GALASACTL_VERSION}'"
 echo "Running command: galasactl ${INPUT_GALASACTL_ARGS}"
 
-galasactl ${INPUT_GALASACTL_ARGS}
+if [ -z "${INPUT_GALASACTL_ARGS}" ]; then
+    ${INPUT_SCRIPT}
+else
+    galasactl ${INPUT_GALASACTL_ARGS}
+fi
